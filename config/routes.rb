c8be43d1d36c6d9ff6748c9fb39b40c278a2b8e8
Chaz Larson <chaz@chazlarson.com>
2018-12-22 02:37:27 +0000

    Add the show method for the user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,2 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-
-  # BEGIN: user section
-  devise_for :users,
-             controllers: { registrations: 'users/registrations',
-                            sessions: 'users/sessions',
-                            passwords: 'users/passwords',
-                            confirmations: 'users/confirmations',
-                            unlocks: 'users/unlocks' }
-  # END: user section
-
-  # BEGIN: static pages
-  root 'static_pages#home'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  # END: static pages
-end
